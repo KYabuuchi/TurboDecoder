a033e90eea93432362e5813f16aa2d6e8edcd0a7
Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
2023-04-10 08:53:18 +0900

    add comparator
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>

--- src/comparator_node.cpp
+++ src/comparator_node.cpp
@@ -30,24 +30,31 @@ private:
 
   void on_compressed_image(const CompressedImage& msg)
   {
+    // NOTE: Place data on cache
+    // (The first process is slower because there is no data on the cache.)
+    cv::imdecode(cv::Mat(msg.data), cv::IMREAD_COLOR);
+
+    // (1) jet_decoder
     jet_decoder::Timer timer;
     cv::Mat image1 = decoder_.decompress_using_cache(msg.data);
     const long time1 = timer.micro_seconds();
 
+    // (2) cv::imdecode
     timer.reset();
     cv::Mat image2 = cv::imdecode(cv::Mat(msg.data), cv::IMREAD_COLOR);
     cv::resize(image2, image2, cv::Size(), 1.0 / denom_, 1.0 / denom_);
     const long time2 = timer.micro_seconds();
 
 
+    // Visualize
     auto put_text = [](cv::Mat image, long time, const std::string& prefix) -> void {
       std::stringstream ss;
       ss << prefix << " " << std::fixed << std::setprecision(3) << (time / 1000.0) << " [ms]";
-      cv::putText(image, ss.str(), cv::Point(20, 50), cv::FONT_HERSHEY_PLAIN, 3, cv::Scalar(0, 255, 255), 3);
+      cv::putText(image, ss.str(), cv::Point(20, 50), cv::FONT_HERSHEY_PLAIN, 2, cv::Scalar(0, 255, 255), 2);
     };
 
-    put_text(image1, time1, "jet");
-    put_text(image2, time2, "imdecode");
+    put_text(image1, time1, "jet_decode");
+    put_text(image2, time2, "cv::imdecode");
     RCLCPP_INFO_STREAM(get_logger(), "jet: " << time1 / 1000. << " imdecode:" << time2 / 1000.);
 
     cv::Mat concat_image;
