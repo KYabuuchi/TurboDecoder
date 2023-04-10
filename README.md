456ff4dff01be026fdd9116f47f64866ce97642b
Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
2023-04-10 19:47:29 +0900

    add launch file
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # TurboDecoder
 
+## How to build
+
+I believe the following command will install `libjpeg-turbo`
+
+`rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO`.
+
+<details><summary>how to install libjepeg-turbo manually</summary><div>
+
 `sudo apt install libturbojpeg libturbojpeg0-dev`
+
+</div></details>
+
+## How to run
+
+#### x1 decompression with visualization
+
+`ros2 launch turbo_decoder compare.launch.xm show:=true`
+
+#### 1/2 decompression
+
+`ros2 launch turbo_decoder compare.launch.xml scale_denom:=4`
+
+#### 1/4 decompression with visualization
+
+`ros2 launch turbo_decoder compare.launch.xml scale_denom:=4 show:true`
--- src/decoder_node.cpp
+++ src/decoder_node.cpp
@@ -6,33 +6,54 @@
 
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 class Decoder : public rclcpp::Node
 {
 public:
   using CompressedImage = sensor_msgs::msg::CompressedImage;
-  Decoder() : Node("jet_decoder")
+
+  Decoder() : Node("jet_decoder"),
+              use_imdecode_(declare_parameter<bool>("use_imdecode", false)),
+              use_imshow_(declare_parameter<bool>("use_imshow", false))
   {
     using std::placeholders::_1;
     auto qos = rclcpp::QoS(10).best_effort();
     auto on_compressed_image = std::bind(&Decoder::on_compressed_image, this, _1);
-    sub_compressed_image_ = create_subscription<CompressedImage>("/sensing/camera/traffic_light/image_raw/compressed", qos, on_compressed_image);
+    sub_compressed_image_ = create_subscription<CompressedImage>("src_image", qos, on_compressed_image);
 
-    decoder_.set_scale(1, 4);
+    {
+      const int num = declare_parameter<int>("scale_num");
+      const int denom = declare_parameter<int>("scale_denom");
+      decoder_.set_scale(num, denom);
+      scale_ratio_ = static_cast<double>(num) / static_cast<double>(denom);
+    }
   }
 
 private:
+  const bool use_imdecode_;
+  const bool use_imshow_;
+  double scale_ratio_;
   turbo_decoder::TurboDecoder decoder_;
   rclcpp::Subscription<CompressedImage>::SharedPtr sub_compressed_image_;
 
   void on_compressed_image(const CompressedImage& msg)
   {
     jet_decoder::Timer timer;
-    cv::Mat image = decoder_.decompress_using_cache(msg.data);
+    cv::Mat image;
+
+    if (use_imdecode_) {
+      image = cv::imdecode(cv::Mat(msg.data), cv::IMREAD_COLOR);
+      cv::resize(image, image, cv::Size(), scale_ratio_, scale_ratio_);
+    } else {
+      image = decoder_.decompress_using_cache(msg.data);
+    }
     RCLCPP_INFO_STREAM(get_logger(), "image decompressed: " << timer);
 
-    // cv::imshow("show", image);
-    // cv::waitKey(10);
+    if (use_imshow_) {
+      cv::imshow("decompressed", image);
+      cv::waitKey(10);
+    }
   }
 };
 
